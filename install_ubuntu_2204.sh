c98f21b249fbb6d03ae14b69bee72beec020ef24
Brandon Long <brandongraylong@gmail.com>
2023-06-11 22:02:02 -0500

    Updated install script to work on first and every build thereafter along with documentation.

--- install_ubuntu_2204.sh
+++ install_ubuntu_2204.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+BUILD_DIRECTORY="build"
+
 sudo apt install cmake g++ build-essential gnuradio libsndfile1-dev -y
-rm -rf build
-mkdir build
-cd build
+
+if [ -d "$BUILD_DIRECTORY" ]; then
+  cd "$BUILD_DIRECTORY"
+  make uninstall
+else
+  mkdir "$BUILD_DIRECTORY"
+  cd "$BUILD_DIRECTORY"
+fi
+
 cmake ..
 make
 sudo make install
